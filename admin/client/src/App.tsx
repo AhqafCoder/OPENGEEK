a1178eca8ae8668a2344d476d7d945823514ef94
Vivek Vishwakarma <90537761+vivek12coder@users.noreply.github.com>
2025-08-10 16:28:05 +0530

    Revert "Vivek"

--- admin/client/src/App.tsx
+++ admin/client/src/App.tsx
@@ -1,35 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AdminLayout from '@/components/layout/AdminLayout';
-import LoginPage from '@/components/auth/LoginPage';
-import Dashboard from '@/components/dashboard/Dashboard';
-import UserList from '@/components/users/UserList';
-import UserForm from '@/components/users/UserForm';
-import ContentList from '@/components/content/ContentList';
-import ContentForm from '@/components/content/ContentForm';
-import ProtectedRoute from '@/components/auth/ProtectedRoute';
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/admin/login" element={<LoginPage />} />
-        <Route path="/admin" element={
-          <ProtectedRoute>
-            <AdminLayout />
-          </ProtectedRoute>
-        }>
-          <Route index element={<Dashboard />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="users" element={<UserList />} />
-          <Route path="users/add" element={<UserForm />} />
-          <Route path="users/edit/:id" element={<UserForm />} />
-          <Route path="content" element={<ContentList />} />
-          <Route path="content/add" element={<ContentForm />} />
-          <Route path="content/edit/:id" element={<ContentForm />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  )
 }
 
 export default App
